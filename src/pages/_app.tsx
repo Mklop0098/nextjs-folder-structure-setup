fe436c4a55e83a55541ccca419d156b8dfe3b80e
Mklop0098 <hoangsonfds@gmail.com>
2024-12-03 09:36:48 +0700

    setup

---
+++ src/pages/_app.tsx
@@ -0,0 +1,20 @@
+import { NextPage } from "next";
+import { AppProps } from "next/app";
+import { ReactElement, ReactNode } from "react";
+
+type NextPageWithLayout = NextPage & {
+  getLayout?: (page: ReactElement) => ReactNode;
+};
+
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout;
+};
+
+const App = ({ Component, pageProps }: AppPropsWithLayout) => {
+
+  return (
+    <Component {...pageProps} />
+  )
+}
+
+export default App
---
+++ src/pages/test/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Test() {
+    return (
+        <div>
+            this is app page
+        </div>
+    )
+}
+
+export default Test
